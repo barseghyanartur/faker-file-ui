145c84545daf7c5595d3b3c092361937f4fe503f
Artur Barseghyan <artur.barseghyan@gmail.com>
2023-06-01 21:56:03 +0200

    Add code coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config)
       // implement node event listeners here
+      return config
     },
   },
   projectId: 'faker-file-ui',
