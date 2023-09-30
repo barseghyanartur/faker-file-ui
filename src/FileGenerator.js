ad69bcee0e443a5a83d31b5bfb2c3f94c081b900
Artur Barseghyan <artur.barseghyan@gmail.com>
2023-10-01 00:24:34 +0200

    Clean up

--- src/FileGenerator.js
+++ src/FileGenerator.js
@@ -17,11 +17,6 @@ import {
   Button,
 } from "@mui/material";
 
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
 import Box from "@mui/material/Box";
 import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
